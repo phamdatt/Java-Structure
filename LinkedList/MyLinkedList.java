06af4ac26899bdd8df9ba7be5d30a8efee8f668a
Pham Tien Dat <tiendatt3009@gmail.com>
2022-03-18 09:00:03 +0700

    dev-dat-add-method-linked-list

--- LinkedList/MyLinkedList.java
+++ LinkedList/MyLinkedList.java
@@ -25,38 +25,25 @@ public class MyLinkedList {
 
   public static Node addToHead(Node head, int value) {
     Node newNode = new Node(value);
-    if (head != null) {
-      newNode.next = head;
-    }
-    return newNode;
-  }
-
-  public static Node addToTail(Node head, int value) {
-    Node newNode = new Node(value);
     if (head == null) {
-      return newNode;
+      head = newNode;
     } else {
-      Node lastNode = head;
-      while (lastNode.next != null) {
-        lastNode = lastNode.next;
-      }
-      lastNode.next = newNode;
+      newNode.next = head;
+      head = newNode;
     }
     return head;
   }
 
-  public static Node insertCenterLinkedList(Node head, int value, int index) {
+  public static Node addToAt(Node head, int value, int index) {
     Node newNode = new Node(value);
     if (index == 0) {
       return addToHead(head, value);
     } else {
-      // B1, Tim vi tri can them
       Node currentNode = head;
       int count = 0;
       while (currentNode.next != null) {
         count++;
         if (count == index) {
-          //Thuc hien add
           newNode.next = currentNode.next;
           currentNode.next = newNode;
           break;
@@ -67,13 +54,64 @@ public class MyLinkedList {
     return head;
   }
 
+  public static Node addToTail(Node head, int value) {
+    Node newNode = new Node(value);
+    if (head == null) {
+      head = newNode;
+    } else {
+      Node lastNode = head;
+      while (lastNode.next != null) {
+        lastNode = lastNode.next;
+      }
+      lastNode.next = newNode;
+    }
+    return head;
+  }
+
+  public static Node removeNodeAtHead(Node head) {
+    if (head != null) {
+      head = head.next;
+    }
+    return head;
+  }
+
+  public static Node removeNodeAtTail(Node head) {
+    Node prev = null;
+    Node currentNode = head;
+    while (currentNode.next != null) {
+      prev = currentNode;
+      currentNode = currentNode.next;
+    }
+    prev.next = currentNode.next;
+    return head;
+  }
+
+  public static Node sortLinkedList(Node head) {
+    if (head == null) {
+      return null;
+    } else {
+      Node currentNode = head;
+      Node nextNode = currentNode.next;
+      Node prev = null;
+      if (currentNode.value > nextNode.value) {
+        currentNode.next = nextNode.next; // 2
+        prev = nextNode; // 1
+        System.out.println(prev.value);
+        prev.next = currentNode;
+        head = prev;
+      }
+    }
+    return head;
+  }
+
   public static void main(String[] args) {
-    Node n1 = new Node(1);
-    n1 = addToTail(n1, 3);
-    n1 = addToHead(n1, 0);
-    n1 = addToHead(n1, 3);
-    n1 = addToTail(n1, 7);
-    n1 = insertCenterLinkedList(n1, 9,1);
+    Node n1 = new Node(2);
+    // n1 = addToHead(n1, 0);
+    // n1 = addToAt(n1, 2, 1);
+    n1 = addToTail(n1, 1);
+    // n1 = removeNodeAtHead(n1);
+    // n1 = removeNodeAtTail(n1);
+    n1 = sortLinkedList(n1);
     printLinkedList(n1);
   }
 }
